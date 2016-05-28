347d3d09e074828b05a016907896be79a4dd0d5a
Rhea Rivera <rjuner@gmail.com>
2016-05-28 16:38:55 -0500

    clicks work, need to format clock

--- assets/js/game03.js
+++ assets/js/game03.js
@@ -12,7 +12,7 @@ $("#start").on("click", function(){
 });
 
 function selectAnswer(){
-	$(".btn").on("click", function(){
+	$(".selection").on("click", function(){
 		//console.log(this);
 	if( $(this).text() == questions[questionIndex].correct){
 		rightScreen();
@@ -23,11 +23,9 @@ function selectAnswer(){
 	});
 }
 
-selectAnswer();
-
-// function visualCounter(){
-// 	clock = setInterval()
-// }
+function visualCounter(){
+	clock = setInterval()
+}
 
 function initalState(){
 	questions = [
@@ -35,42 +33,50 @@ function initalState(){
 			q: "Where do bananas grow?", 
 			choices: [
 				"On Trees", "On Herbs", "Under Water", "In the Desert"],
-			correct: "On Herbs"
+			correct: "On Herbs", 
+			reason: "THIS IS THE REASON FOR WHATEVER #1"
 		},
 		{
 			q: "What is Michael J Fox's middle name?", 
 			choices: ["Joseph", "Andrew", "Michael", "James"],
-			correct: "Andrew"
+			correct: "Andrew", 
+			reason: "THIS IS THE REASON FOR WHATEVER #2"
 		}, 
 		{
 			q: "When did Nelson Mandela die?", 
 			choices: ["He's still alive", "2013", "1980", "1999"],
-			correct: "2013"
+			correct: "2013", 
+			reason: "THIS IS THE REASON FOR WHATEVER #3"
 		},
 		{	
 			q: "Which quote is correct?", 
 			choices: ["Luke, I am your father.", "Do you feel lucky, punk?", "Toto, I don't think we're in Kansas anymore.", "Frankly, my dear, I don't give a damn."], 
-			correct: ["Frankly, my dear, I don’t give a damn."]
+			correct: "Frankly, my dear, I don’t give a damn.", 
+			reason: "THIS IS THE REASON FOR WHATEVER #4"
 		}, 
 		{
 			q: "What percent of our brain do we really use?", 
 			choices: ["10%", "90%", "We use different parts for different functions.", "50%"], 
-			correct: "We use different parts for different functions."
+			correct: "We use different parts for different functions.", 
+			reason: "THIS IS THE REASON FOR WHATEVER #5"
 		}, 
 		{
 			q: "How many senses do we have?", 
 			choices: ["6", "5", "20+", "10"], 
-			correct: "20+"
+			correct: "20+", 
+			reason: "THIS IS THE REASON FOR WHATEVER #6"
 		}, 
 		{
 			q: "Heroin has NOT been marketed as a remedy to which of the following?", 
 			choices: ["Children's Cough", "The Common Cold", "Opiate Addiction", "Sleeplessness"], 
-			correct: "Sleeplessness"
+			correct: "Sleeplessness", 
+			reason: "THIS IS THE REASON FOR WHATEVER #7"
 		}, 
 		{
 			q: "Which of the following is spelled incorrectly?", 
 			choices: ["Brussels Sprouts", "Berernstain Bears", "Millenium", "Disburse"], 
-			correct: "Millenium"
+			correct: "Millenium", 
+			reason: "THIS IS THE REASON FOR WHATEVER #8"
 		}
 
 	];
@@ -81,17 +87,20 @@ function initalState(){
 }
 
 function next(){
-	timeUp = setTimeout(wrongScreen, 1000 * 5);
+	timeUp = setTimeout(outofTime, 1000 * 5);
 	questionIndex++;
 	askQuestion();
 	makeButtons();
+	selectAnswer();
 }
 
 function askQuestion(){
 	$("#questionArea").html(questions[questionIndex].q);
+	$("#flavorText").hide();
 }
 
 function makeButtons(){
+	$("#buttonHolder").show(); 
 	$("#button01").html(questions[questionIndex].choices[0]);
 	$("#button02").html(questions[questionIndex].choices[1]);
 	$("#button03").html(questions[questionIndex].choices[2]);
@@ -100,21 +109,38 @@ function makeButtons(){
 
 function wrongScreen(){
 	clearTimeout(timeUp);
-	alert("WRONG!");
-	//next();
+
+	$("#buttonHolder").hide().off("click");
+
+	$("#flavorText").show().html("WRONG! <br>" + "Here's why: <br>" + questions[questionIndex].reason);
+
+	// in 5 seconds it will go to next question. 
+	timeUp = setTimeout(next, 1000 * 5);
+}
+
+function outofTime(){
+	clearTimeout(timeUp);
+
+	$("#buttonHolder").hide().off("click");
+
+	$("#flavorText").show().html("OUT OF TIME! <br>" + "Here's why: <br>" + questions[questionIndex].reason);
+
+	// in 5 seconds it will go to next question. 
+	timeUp = setTimeout(next, 1000 * 5);
 }
 
 function rightScreen(){
 	clearTimeout(timeUp);
-	$("#rightBox").empty("<button>"); 
-	$("#rightBox").append("<h1> YOU DID IT!</h1>"); 
-	alert("YEAHHHH!");
+
+	$("#buttonHolder").hide().off("click");
+
+	$("#flavorText").show().html("YESSSS! <br>" + "Here's why: <br>" + questions[questionIndex].reason);
+
 	// in 5 seconds it will go to next question. 
-	timeUp = setTimeout(next, 1000 * 2);
+	timeUp = setTimeout(next, 1000 * 5);
 }
 
 initalState();
-
 
 // ***************************************************************
 // STOP! BELOW IS THE END OF THE DOCUMENT READY FUNCTION! 
